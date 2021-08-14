b281ad125e94e0c2afd86c160e1c70115c26e755
Jacob <43463199+j6i@users.noreply.github.com>
2021-08-13 18:57:00 -0700

    accept eth
    added the ability to accept ether

--- app/deploy.js
+++ app/deploy.js
@@ -3,9 +3,12 @@ import {ethers} from 'ethers';
 
 const provider = new ethers.providers.Web3Provider(ethereum);
 
-export default async function deploy(arbiter, beneficiary, value) {
+export default async function deploy(arbiter, beneficiary, value1) {
   await ethereum.request({ method: 'eth_requestAccounts' });
   const signer = provider.getSigner();
   const factory = new ethers.ContractFactory(Escrow.abi, Escrow.bytecode, signer);
-  return factory.deploy(arbiter, beneficiary, { value });
+  console.log(value1);
+  return factory.deploy(arbiter, beneficiary, {
+    value: ethers.utils.parseEther(value1)
+   });
 }
